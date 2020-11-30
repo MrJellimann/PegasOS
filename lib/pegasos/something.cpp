d445180e0fb9ffa9b0966f3373efb6a78e298a75
MrJellimann <46575936+MrJellimann@users.noreply.github.com>
2020-11-30 08:04:18 -0500

    rebased off of commit 916c763

--- lib/pegasos/something.cpp
+++ lib/pegasos/something.cpp
@@ -3,7 +3,7 @@
 void awful_funct()
 {
     // This function does nothing other than allocate an integer
-    int number = 10;
+    //int number = 10;
 
     // It is for demonstration purposes and acts as a placeholder to be compiled for the /lib/pegasos folder
     // so that I can link it up to the rest of the systems here in circle
